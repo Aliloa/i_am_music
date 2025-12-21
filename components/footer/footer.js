a48cdee338e534f3f9600cf3e7d55663c3a9e405
marineslimane <marineslimane05@gmail.com>
2025-12-21 23:14:25 +0100

    finitions

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,7 +1,7 @@
 const footer_container = document.getElementById("footer-container");
 
 if (footer_container) {
-  fetch('../components/footer/footer.html')
+  fetch('components/footer/footer.html')
     .then(res => res.text())
     .then(html => footer_container.innerHTML = html)
     .catch(err => console.error("Erreur footer:", err));
